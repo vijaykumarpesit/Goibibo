4a19abadb3fd7da960fab53cfab686bfacc9a264
Vijay <vijayakumar@hike.in>
2015-09-22 09:39:31 +0530

    Add basic project file and view controller

---
+++ GoIbibo/GoHomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoIbibo
+//
+//  Created by Vijay on 22/09/15.
+//  Copyright © 2015 Vijay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoHomeViewController : UIViewController
+
+
+@end
+
---
+++ GoIbibo/GoHomeViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GoIbibo
+//
+//  Created by Vijay on 22/09/15.
+//  Copyright © 2015 Vijay. All rights reserved.
+//
+
+#import "GoHomeViewController.h"
+
+@interface GoHomeViewController ()
+
+@end
+
+@implementation GoHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
