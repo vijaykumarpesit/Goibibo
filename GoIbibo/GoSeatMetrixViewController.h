01f37193ddd37c7973ff723492847668aa17509e
Vijay <vijaykumar.pesit@gmail.com>
2015-09-24 01:33:44 +0530

    Add seat matrix

---
+++ GoIbibo/GoSeatMetrixViewController.h
@@ -0,0 +1,13 @@
+//
+//  GoSeatMetrixViewController.h
+//  GoIbibo
+//
+//  Created by Vijay on 24/09/15.
+//  Copyright © 2015 Vijay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoSeatMetrixViewController : UIViewController
+
+@end
