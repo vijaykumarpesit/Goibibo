1c52d214d4c3b0619eab469c4bc45dc3ae2daa05
Vijay <vijaykumar.pesit@gmail.com>
2015-10-18 10:13:52 +0530

    List buses

--- GoIbibo/GoFriendsTripDetailsController.m
+++ GoIbibo/GoFriendsTripDetailsController.m
@@ -8,10 +8,14 @@
 
 #import "GoFriendsTripDetailsController.h"
 #import "GoBusInfoCell.h"
+#import <parse/parse.h>
+#import "GoUserModelManager.h"
+#import "GoContactSyncEntry.h"
+#import "GoContactSync.h"
 
 @interface GoFriendsTripDetailsController ()
 
-@property (nonatomic, strong) NSMutableArray *friendsTripsDetails;
+@property (nonatomic, strong) NSMutableArray *friendsList;
 
 @end
 
@@ -19,6 +23,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.friendsList = [[NSMutableArray alloc] init];
     
     [self.tableView registerNib:[UINib nibWithNibName:@"GoBusInfoCell" bundle:nil] forCellReuseIdentifier:@"FriendsTripsDetailsCellIdentifier"];
     // Uncomment the following line to preserve selection between presentations.
@@ -26,6 +31,7 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [self checkAndConfigureFriends];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,16 +46,31 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.friendsTripsDetails.count;
+    return self.friendsList.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     GoBusInfoCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FriendsTripsDetailsCellIdentifier" forIndexPath:indexPath];
     
-    cell.travellerName.text = [self.friendsTripsDetails[indexPath.row] valueForKey:@""];
-    cell.minimumFare.text = [self.friendsTripsDetails[indexPath.row] valueForKey:@""];
-    cell.busTypeName.text = [self.friendsTripsDetails[indexPath.row] valueForKey:@""];
+    NSMutableString *sourceDest = [NSMutableString stringWithString:[self.friendsList[indexPath.row] valueForKey:@"source"]];
+    [sourceDest appendString:[NSString stringWithFormat:@"--->%@",[self.friendsList[indexPath.row] valueForKey:@"destination"]]];
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyyy-MM-dd"];
+    NSDate *departureDate = [self.friendsList[indexPath.row] valueForKey:@"departureDate"];
+    NSString *dateString = [formatter stringFromDate:departureDate];
+    
+    cell.travellerName.text = [self.friendsList[indexPath.row] valueForKey:@"passengerName"];
+    cell.minimumFare.text = dateString;
+    cell.busTypeName.text = sourceDest;
+    cell.busTypeName.font = cell.travellerName.font;
+    [cell.departureToArrivalTime setHidden:YES];
+    [cell.availableSeats setHidden:YES];
     return cell;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return  90;
 }
 
 /*
@@ -85,5 +106,41 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)checkAndConfigureFriends {
+    
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        PFQuery *query = [PFQuery queryWithClassName:@"BusBookingDetails"];
+        [query whereKey:@"departureDate" greaterThanOrEqualTo:[NSDate date]];
+        
+        NSString *myNumber = [[[GoUserModelManager sharedManager] currentUser] phoneNumber];
+        [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+            
+            [objects enumerateObjectsUsingBlock:^(PFObject *  _Nonnull object, NSUInteger idx, BOOL * _Nonnull stop) {
+                
+                NSString *phoneNo = object[@"bookedUserPhoneNo"];
+                
+                GoContactSyncEntry *entry =[[[GoContactSync sharedInstance] syncedContacts] valueForKey:phoneNo];
+                
+                if (entry && ![phoneNo isEqualToString:myNumber]) {
+                    
+                    NSMutableDictionary *mutableDict = [[NSMutableDictionary alloc] init];
+                    for(id key in [object allKeys]) {
+                        [mutableDict setValue:object[key] forKey:key];
+                    }
+                    [mutableDict setValue:entry.name forKey:@"passengerName"];
+                    [self.friendsList addObject:mutableDict];
+                }
+            }];
+            
+            NSLog(@"Friends count %lu",(unsigned long)self.friendsList.count);
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.tableView reloadData];
+            });
+        }];
+        
+    });
+}
+
 
 @end
